0a494c9c0df424d99fbb432ef904f9a4b2fa04e4
Jerome Tan <jerome.tan@tuta.io>
2022-02-16 16:21:23 +0100

    Week6Day3 exo 2 yelp copycat

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'starting seed...'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+Review.destroy_all
+
+puts 'Creating restaurants and reviews...'
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: Restaurant::CATEGORIES.sample
+  )
+  restaurant.save!
+  puts "Created #{restaurant.name}"
+  rand(3..17).times do
+    review = Review.new(
+      rating: rand(0..5),
+      content: Faker::Restaurant.review,
+      restaurant_id: restaurant.id
+    )
+    review.save!
+  end
+  puts "Created #{restaurant.reviews.count} reviews for #{restaurant.name}"
+end
+
+puts 'Done seeding!'
